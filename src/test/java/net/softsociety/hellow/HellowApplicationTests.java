4de1b989d7b50f5676735ed2f987901ea3af4648
박성우\user <tjddn7500@gmail.com>
2023-03-05 19:47:01 +0900

    asdasdas

---
+++ src/test/java/net/softsociety/hellow/HellowApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.hellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
